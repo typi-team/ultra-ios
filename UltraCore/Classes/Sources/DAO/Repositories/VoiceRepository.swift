f3d0553c168d09748cc00cb7e43e61f69f3f08e4
Baglan <b.daribayev@typi.team>
2024-01-16 16:04:50 +0600

    Fix for audio pause

--- UltraCore/Classes/Sources/DAO/Repositories/VoiceRepository.swift
+++ UltraCore/Classes/Sources/DAO/Repositories/VoiceRepository.swift
@@ -9,13 +9,19 @@ import Foundation
 import AVFAudio
 import RxSwift
 
-class VoiceItem {
+class VoiceItem: CustomStringConvertible {
     let voiceMessage: VoiceMessage
     var currentTime: TimeInterval = 0.0
-    
-    init(voiceMessage: VoiceMessage, currentTime: TimeInterval) {
+    var isPlaying: Bool
+
+    init(voiceMessage: VoiceMessage, currentTime: TimeInterval, isPlaying: Bool) {
         self.voiceMessage = voiceMessage
         self.currentTime = currentTime
+        self.isPlaying = isPlaying
+    }
+
+    var description: String {
+        return "[VoiceItem]: \(voiceMessage.fileID) \(currentTime) / \(voiceMessage.duration) isPlaying: \(isPlaying)"
     }
 }
 
@@ -38,7 +44,24 @@ class VoiceRepository: NSObject {
         self.timer?.invalidate()
         try? AVAudioSession.sharedInstance().setActive(false)
     }
-    
+
+    func playPause() {
+        if audioPlayer?.isPlaying ?? false {
+            audioPlayer?.pause()
+        } else {
+            audioPlayer?.play()
+        }
+        var currentItem = try? currentVoice.value()
+        currentItem?.isPlaying = audioPlayer?.isPlaying ?? false
+        self.currentVoice.on(
+            .next(currentItem)
+        )
+    }
+
+    func pause() {
+        audioPlayer?.pause()
+    }
+
     func play(message: Message, atTime: TimeInterval = .zero) {
         guard let soundURL = self.mediaUtils.mediaURL(from: message) else { return }
         do {
@@ -53,7 +76,9 @@ class VoiceRepository: NSObject {
             
             self.timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
             self.audioPlayer = audioPlayer
-            self.currentVoice.on(.next(.init(voiceMessage: message.voice, currentTime: atTime)))
+            self.currentVoice.on(
+                .next(.init(voiceMessage: message.voice, currentTime: atTime, isPlaying: audioPlayer.isPlaying))
+            )
         } catch {
             self.stop()
             PP.error(error.localizedDescription)
