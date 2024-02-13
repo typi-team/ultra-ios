ca55bb786874080555c709a04f038baa87742c63
Baglan <b.daribayev@typi.team>
2024-02-13 20:13:05 +0600

    Localizable

--- UltraCore/Classes/Sources/Utils/LocalizationEnums/ContactsStrings.swift
+++ UltraCore/Classes/Sources/Utils/LocalizationEnums/ContactsStrings.swift
@@ -9,10 +9,26 @@ import Foundation
 
 enum ContactsStrings: String, StringLocalizable {
     case newChat
-    case was
-    case justNow
-    case backward
-    case yesterday
+    
+    case wasMonths1 = "was.months.1"
+    case wasMonths2 = "was.months.2"
+    case wasMonths5 = "was.months.5"
+    
+    case wasDays1 = "was.days.1"
+    case wasDays2 = "was.days.2"
+    case wasDays5 = "was.days.5"
+    
+    case wasHours1 = "was.hours.1"
+    case wasHours2 = "was.hours.2"
+    case wasHours5 = "was.hours.5"
+    
+    case wasMinutes1 = "was.minutes.1"
+    case wasMinutes2 = "was.minutes.2"
+    case wasMinutes5 = "was.minutes.5"
+    
+    case wasJustNow = "was.justNow"
+    case wasYesterday = "was.yesterday"
+    case wasLongTimeAgo = "was.longTimeAgo"
     
     case yourContactListIsEmpty
     case unfortunately
