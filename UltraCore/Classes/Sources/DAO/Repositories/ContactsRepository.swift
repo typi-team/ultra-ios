c834e5cdadd84dc4a4130d35f9d00f7b01faee58
Baglan <b.daribayev@typi.team>
2024-05-10 18:19:02 +0500

    Contact

--- UltraCore/Classes/Sources/DAO/Repositories/ContactsRepository.swift
+++ UltraCore/Classes/Sources/DAO/Repositories/ContactsRepository.swift
@@ -60,12 +60,11 @@ extension Realm {
             schemaVersion: 4) { migration, oldSchemaVersion in
                 if oldSchemaVersion < 4 {
                     migration.enumerateObjects(ofType: DBConversation.className()) { oldObject, newObject in
-                        print(oldObject!["contact"])
-                        if let contact = oldObject!["contact"] as? DBContact {
-                            let list = List<DBContact>()
-                            list.append(contact)
-                            newObject!["contact"] = list
-                        }
+                        print(oldObject!["contact"] as! DBContact)
+                        let contact = oldObject!["contact"] as! DBContact
+                        let list = List<DBContact>()
+                        list.append(contact)
+                        newObject!["contact"] = list
                     }
                 }
             }
