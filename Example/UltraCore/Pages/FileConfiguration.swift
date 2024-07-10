fe4930cb5737fd7d4a936ef45cb8f09c0315d084
Yerke <yerkebulan.amankul.99@gmail.com>
2024-07-10 15:34:52 +0500

    remove unused file

--- Example/UltraCore/Pages/FileConfiguration.swift
+++
@@ -1,9 +0,0 @@
-//
-//  FileConfiguration.swift
-//  UltraCore_Example
-//
-//  Created by Yerkebulan Amankul on 10.07.2024.
-//  Copyright © 2024 CocoaPods. All rights reserved.
-//
-
-import Foundation
